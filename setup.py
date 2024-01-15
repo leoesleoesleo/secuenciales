cd898a7cfc1b5a16b63210907811f0ae9744c03b
Leo <44104335+leoesleoesleo@users.noreply.github.com>
2024-01-15 18:16:23 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='secuenciales',
+    version='0.1',
+    description='Un script para ejecutar secuencialmente los JOBS',
+    author='Leonardo GBM',
+    packages=find_packages(),
+    install_requires=[
+        # Lista de dependencias si las hay
+    ],
+    scripts=['secuenciales.py'],
+    license='MIT',
+    url='https://github.com/leoesleoesleo/secuenciales',
+)
